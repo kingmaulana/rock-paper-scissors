d44a3584a72e8aa2be1a958d8d5e4ba3bf7b3706
Maulana-king <agusrohim56@gmail.com>
2024-10-06 12:50:06 +0700

    add main function to hold all the logic, make loop to run game for 5 round and show the winner from the score

--- rpp.js
+++ rpp.js
@@ -1,64 +1,80 @@
 
-//function return random pick 'rock' 'paper' 'scissor'
-function getComputerChoice() {
-    const randomPick = Math.floor(Math.random() * 3);
+
+
+function playGame() {
+
+    //function return random pick 'rock' 'paper' 'scissor'
+    function getComputerChoice() {
+        const randomPick = Math.floor(Math.random() * 3);
+        
+        switch (randomPick) {
+            case 0:
+                return 'rock'
+            case 1:
+                return 'paper'
+            default:
+                return 'scissor'
+        }
+    }
+
+    //function show input alert to user
+    function getHumanChoice() {
+        let humanInput = prompt("Pick one: ROCK, PAPER or SCISSOR ");
+        //making any choice from human to case-insensitive
+        return humanInput.toLowerCase();
+    }
+
+    //track score both user and computer
+    let humanScore = 0;
+    let computerScore = 0;
+
+    //Make 5 round to start over every time input send
+    for(let i = 0; i < 5; i++) {
+        //function for computer and human choice at the game while playing
+        function playAround(humanChoice, computerChoice) {
+            const human = humanChoice;
+            const comp = computerChoice;
     
-    switch (randomPick) {
-        case 0:
-            return 'rock'
-        case 1:
-            return 'paper'
-        default:
-            return 'scissor'
+            // console.log(human);
+            // console.log(comp);
+    
+            if(human == 'rock' && comp == 'rock' || human == 'paper' && comp == 'paper' || human == 'scissor' && comp == 'scissor'){
+                console.log('Draw, play again.');
+            } else if (human == 'rock' && comp == 'paper') {
+                console.log('You lose!, Paper beats Rock.');
+                return computerScore += 1;
+            } else if (human == 'rock' && comp == 'scissor'){
+                console.log('You Win!, Rock beats Scissor.');
+                return humanScore += 1;
+            } else if (human == 'paper' && comp == 'rock') {
+                console.log('You Win!, Paper beats Rock.');
+                return humanScore += 1;
+            } else if (human == 'paper' && comp == 'scissor') {
+                console.log('You lose!, Scissor beats Paper.');
+                return computerScore += 1;
+            } else if (human == 'scissor' && comp == 'rock') {
+                console.log('You lose!, Rock beats Scissor.');
+                return computerScore += 1;
+            } else if (human == 'scissor' && comp == 'paper'){
+                console.log('You Win!, Scissor beats Paper.');
+                return humanScore += 1;
+            } else {
+                console.log('You dont pick one of the option correctly!')
+            }
+        }
+
+        const humanSelection = getHumanChoice();
+        const computeSelection = getComputerChoice();
+
+        playAround(humanSelection, computeSelection);
     }
-}
-
-//function show input alert to user
-function getHumanChoice() {
-    let humanInput = prompt("Pick one: ROCK, PAPER or SCISSOR ");
-    //making any choice from human to case-insensitive
-    return humanInput.toLowerCase();
-}
-
-//track score both user and computer
-let humanScore = 0;
-let computerScore = 0;
-
-
-//function for computer and human choice at the game while playing
-function playAround(humanChoice, computerChoice) {
-    const human = humanChoice;
-    const comp = computerChoice;
-
-    // console.log(human);
-    // console.log(comp);
-
-    if(human == 'rock' && comp == 'rock' || human == 'paper' && comp == 'paper' || human == 'scissor' && comp == 'scissor'){
-        console.log('Draw, play again.');
-    } else if (human == 'rock' && comp == 'paper') {
-        console.log('You lose!, Paper beats Rock.');
-        return computerScore += 1;
-    } else if (human == 'rock' && comp == 'scissor'){
-        console.log('You Win!, Rock beats Scissor.');
-        return humanScore += 1;
-    } else if (human == 'paper' && comp == 'rock') {
-        console.log('You Win!, Paper beats Rock.');
-        return humanScore += 1;
-    } else if (human == 'paper' && comp == 'scissor') {
-        console.log('You lose!, Scissor beats Paper.');
-        return computerScore += 1;
-    } else if (human == 'scissor' && comp == 'rock') {
-        console.log('You lose!, Rock beats Scissor.');
-        return computerScore += 1;
-    } else if (human == 'scissor' && comp == 'paper'){
-        console.log('You Win!, Scissor beats Paper.');
-        return humanScore += 1;
+    
+    if(humanScore > computerScore) {
+        console.log("You're Win! this game with score " + "You: " + humanScore + " VS Computer " +  computerScore)
     } else {
-        console.log('You dont pick one of the option correctly!')
+        console.log("You're Lose! this game with score " + "You: " + humanScore + " VS Computer " +  computerScore)
     }
+    
 }
 
-const humanSelection = getHumanChoice();
-const computeSelection = getComputerChoice();
-
-playAround(humanSelection, computeSelection);
+playGame();

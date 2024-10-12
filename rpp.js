ad04ff577c10b32e4ae1680bc0a74c42f0066ca3
Maulana-king <agusrohim56@gmail.com>
2024-10-12 08:46:55 +0700

    remove function to loop 5 round

--- rpp.js
+++ rpp.js
@@ -29,45 +29,45 @@ function playGame() {
     let computerScore = 0;
 
     //Make 5 round to start over every time input send
-    for(let i = 0; i < 5; i++) {
-        //function for computer and human choice at the game while playing
-        function playAround(humanChoice, computerChoice) {
-            const human = humanChoice;
-            const comp = computerChoice;
-    
-            // console.log(human);
-            // console.log(comp);
-    
-            if(human == 'rock' && comp == 'rock' || human == 'paper' && comp == 'paper' || human == 'scissor' && comp == 'scissor'){
-                console.log('Draw, play again.');
-            } else if (human == 'rock' && comp == 'paper') {
-                console.log('You lose!, Paper beats Rock.');
-                return computerScore += 1;
-            } else if (human == 'rock' && comp == 'scissor'){
-                console.log('You Win!, Rock beats Scissor.');
-                return humanScore += 1;
-            } else if (human == 'paper' && comp == 'rock') {
-                console.log('You Win!, Paper beats Rock.');
-                return humanScore += 1;
-            } else if (human == 'paper' && comp == 'scissor') {
-                console.log('You lose!, Scissor beats Paper.');
-                return computerScore += 1;
-            } else if (human == 'scissor' && comp == 'rock') {
-                console.log('You lose!, Rock beats Scissor.');
-                return computerScore += 1;
-            } else if (human == 'scissor' && comp == 'paper'){
-                console.log('You Win!, Scissor beats Paper.');
-                return humanScore += 1;
-            } else {
-                console.log('You dont pick one of the option correctly!')
-            }
+
+    //function for computer and human choice at the game while playing
+    function playAround(humanChoice, computerChoice) {
+        const human = humanChoice;
+        const comp = computerChoice;
+
+        // console.log(human);
+        // console.log(comp);
+
+        if(human == 'rock' && comp == 'rock' || human == 'paper' && comp == 'paper' || human == 'scissor' && comp == 'scissor'){
+            console.log('Draw, play again.');
+        } else if (human == 'rock' && comp == 'paper') {
+            console.log('You lose!, Paper beats Rock.');
+            return computerScore += 1;
+        } else if (human == 'rock' && comp == 'scissor'){
+            console.log('You Win!, Rock beats Scissor.');
+            return humanScore += 1;
+        } else if (human == 'paper' && comp == 'rock') {
+            console.log('You Win!, Paper beats Rock.');
+            return humanScore += 1;
+        } else if (human == 'paper' && comp == 'scissor') {
+            console.log('You lose!, Scissor beats Paper.');
+            return computerScore += 1;
+        } else if (human == 'scissor' && comp == 'rock') {
+            console.log('You lose!, Rock beats Scissor.');
+            return computerScore += 1;
+        } else if (human == 'scissor' && comp == 'paper'){
+            console.log('You Win!, Scissor beats Paper.');
+            return humanScore += 1;
+        } else {
+            console.log('You dont pick one of the option correctly!')
         }
-
-        const humanSelection = getHumanChoice();
-        const computeSelection = getComputerChoice();
-
-        playAround(humanSelection, computeSelection);
     }
+
+    const humanSelection = getHumanChoice();
+    const computeSelection = getComputerChoice();
+
+    playAround(humanSelection, computeSelection);
+
     
     if(humanScore > computerScore) {
         console.log("You're Win! this game with score " + "You: " + humanScore + " VS Computer " +  computerScore)
